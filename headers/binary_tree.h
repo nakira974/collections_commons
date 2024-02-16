84030beb5b4242553d0309c60785bb341ab1030e
nakira974 <maxime.loukhal@gmail.com>
2024-02-16 23:19:14 +0100

    Update comments

--- headers/binary_tree.h
+++ headers/binary_tree.h
@@ -7,6 +7,8 @@
 #include "binary_tree_node.h"
 #include "queue.h"
 
+// Fonction pour obtenir le niveau de l'arbre binaire de manière ordonnée
 int** levelOrder(struct TreeNode* root, int* returnSize,
                  int** returnColumnSizes);
+
 #endif //COLLECTIONS_COMMONS_BINARY_TREE_H
