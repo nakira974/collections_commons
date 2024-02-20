7061c054d605db813e4cca7f196b3d9dfdd2de81
nakira974 <maxime.loukhal@gmail.com>
2024-02-20 16:16:01 +0100

    Created dlist

--- headers/dlist.h
+++ headers/dlist.h
@@ -2,34 +2,39 @@
 // Created by maxim on 20/02/2024.
 //
 
-#ifndef COLLECTIONS_COMMONS_LIST_H
-#define COLLECTIONS_COMMONS_LIST_H
+#ifndef COLLECTIONS_COMMONS_DLIST_H
+#define COLLECTIONS_COMMONS_DLIST_H
+
 #include "stdlib.h"
+#include "string.h"
 
 /**
- * Data structure definition for a simple linked list generic element
+ * Data structure definition for a simple double linked list generic element
  */
-typedef struct LinkedElement {
+typedef struct DLinkedElement {
     /**
-     * Generic value of the current element
+     * Reference to the value of the current list element
      */
     void * value;
     /**
-     * Next value of the current element
+     * Reference of the previous element of the current list element
      */
-    struct LinkedElement *next;
-} LinkedElement;
+    struct DLinkedElement *previous;
+    /**
+     * Reference to the next element of the current list element
+     */
+    struct DLinkedElement *next;
+} DLinkedElement;
 
 /**
- * Data structure definition for generic linked list type
+ * Data structure definition for generic double linked list type
  */
-typedef struct LinkedList{
-    /**
-     * Current size of the list
-     */
+typedef struct DLinkedList{
+    /**Current size of the list*/
     int size;
     /**
      * Match handle
+     *
      * @param val1 Left value to compare
      * @param val2 Right value to compare
      * @return true if left is equals to right, otherwise false
@@ -41,17 +46,14 @@ typedef struct LinkedList{
      */
     void (*destroy)(void *value);
     /**
-     *  First element of the list
+     * First element of the list
      */
-    LinkedElement  *head;
+    DLinkedElement  *head;
     /**
      * Last element of the list
      */
-    LinkedElement *tail;
-} LinkedList;
-
-/* ----- PUBLIC DEFINITIONS ----- */
-
+    DLinkedElement *tail;
+} DLinkedList;
 /**
  * Creates a linked list that can be used for other operations
  *
@@ -60,15 +62,15 @@ typedef struct LinkedList{
  * @complexity O(1)
  * @see void list_destroy(LinkedList * list)
  */
-void list_create(LinkedList * list, void( * destroy)(void * value));
+void dlist_create(DLinkedList * list, void( * destroy)(void * value));
+
 
 /**
  * Destroy the specified list, after the call no other further operations will be permit
  * @param list Reference of the list to destroy otherwise false
  * @complexity O(n) where n is the number of elements in the current list
  */
-void list_destroy(LinkedList * list);
-
+void dlist_destroy(DLinkedList * list);
 
 /**
  * Insert a new element just after element parameter
@@ -79,7 +81,18 @@ void list_destroy(LinkedList * list);
  * @return true if the element was added to the current list, otherwise false
  *
  */
-bool list_add(LinkedList *list, LinkedElement* element, const void *value);
+bool dlist_add(DLinkedList *list, DLinkedElement * element, const void *value);
+
+/**
+ * Insert a new element just after element parameter
+ * @param list Reference of the list to add an element
+ * @param element Reference element of the current list to add after
+ * @param value A generic data to add after the element parameter
+ * @complexity O(1)
+ * @return true if the element was added to the current list, otherwise false
+ *
+ */
+bool dlist_add_before(DLinkedList *list, DLinkedElement * element, const void *value);
 
 /**
  * Remove from an element from the current list, then returns a pointer on the value of the deleted element
@@ -89,49 +102,49 @@ bool list_add(LinkedList *list, LinkedElement* element, const void *value);
  * @complexity O(1)
  * @return true if the element was correctly removed, otherwise false
  */
-bool list_remove(LinkedList * list, LinkedElement * element, void **value);
+bool dlist_remove(DLinkedList * list, DLinkedElement * element, void **value);
 
 /***
  * Macro that evaluates the number of elements inside the specified list
  * @return The current element count of the current list
  * @complexity O(1)
  */
-#define list_size(list) ((list)->size)
+#define dlist_size(list) ((list)->size)
 
 /***
  * Macro that evaluates the first element of the specified list
  * @return The first element of the current list
  * @complexity O(1)
  */
-#define list_first(list) ((list)->head)
+#define dlist_first(list) ((list)->head)
 
 /***
  * Macro that evaluates the last element of the specified list
  * @return The last element of the current list
  * @complexity O(1)
  */
-#define list_last(list) ((list)->tail)
+#define dlist_last(list) ((list)->tail)
 
 /***
  * Macro that evaluates if the specified element is the first element of the specified list
  * @return true if the element is the first of the current list, otherwise false
  * @complexity O(1)
  */
-#define list_is_first(list, element) ((element) == (list)->head ? true : false )
+#define dlist_is_first(list, element) ((element)>previous == NULL ? true : false )
 
 /***
  * Macro that evaluates if the specified element is the last element of the specified list
  * @return true if the element is the last of the current list, otherwise false
  * @complexity O(1)
  */
-#define list_is_last(list, element) ((element) == (list)->tail ? true : false )
+#define dlist_is_last(list, element) ((element)>next == NULL ? true : false )
 
 /***
  * Macro that evaluates the value of a list element
  * @return The value stored inside a list element
  * @complexity O(1)
  */
-#define list_value(element) ((element)->value)
+#define dlist_value(element) ((element)->value)
 
 
 /***
@@ -139,6 +152,13 @@ bool list_remove(LinkedList * list, LinkedElement * element, void **value);
  * @return The reference to the next element of the current list element
  * @complexity O(1)
  */
-#define list_next(element) ((element)->next)
+#define dlist_next(element) ((element)->next)
 
-#endif //COLLECTIONS_COMMONS_LIST_H
+/***
+ * Macro that evaluates the previous element of the current list element
+ * @return The reference to the next element of the current list element
+ * @complexity O(1)
+ */
+#define dlist_previous(element) ((element)->previous)
+
+#endif //COLLECTIONS_COMMONS_DLIST_H
--- src/list.c
+++ src/list.c
@@ -5,44 +5,44 @@
 #include <memory.h>
 #include "list.h"
 
-void create_list(List * list, void( * destroy)(void * value)){
+void list_create(LinkedList * list, void( * destroy)(void * value)){
     // Init the list
     list->size = 0;
     list->destroy=destroy;
-    list->head=NULL;
-    list->tail=NULL;
+    list->head=nullptr;
+    list->tail=nullptr;
 }
 
-void destroy_list(List * list){
+void list_destroy(LinkedList * list){
     // Remove each elements
     void **value= nullptr;
-    while (size(list)>0) {
-        if(remove(list,NULL,(void **) &value) && list->destroy !=  NULL){
+    while (list_size(list)>0) {
+        if(list_remove(list,nullptr,(void **) &value) && list->destroy !=  nullptr){
             list->destroy(value);
         }
     }
 
-    memset(list, 0, sizeof(List));
+    memset(list, 0, sizeof(LinkedList));
 }
 
-bool add(List *list, LinkedElement* element, const void *value){
+bool list_add(LinkedList *list, LinkedElement* element, const void *value){
     LinkedElement * new_element = nullptr;
     // If we can't allocate to create a new element then return false
-    if((new_element =(LinkedElement * ) malloc(sizeof (LinkedElement)))== NULL){
+    if((new_element =(LinkedElement * ) malloc(sizeof (LinkedElement)))== nullptr){
         return false;
     }
 
     new_element->value = (void * )value;
     // Insert the element inside the current list
-    if(element==NULL){
+    if(element==nullptr){
         // Head insertion
-        if(size(list) == 0) list->tail = new_element;
+        if(list_size(list) == 0) list->tail = new_element;
             new_element->next = list->head;
             list->head = new_element;
 
             // Else where than head insertion
         }else{
-            if(element->next == NULL){
+            if(element->next == nullptr){
                 list->tail=new_element;
             }
             new_element->next = element->next;
@@ -53,27 +53,27 @@ bool add(List *list, LinkedElement* element, const void *value){
         return true;
 }
 
-bool remove(List * list, LinkedElement * element, void **value){
+bool list_remove(LinkedList * list, LinkedElement * element, void **value){
     LinkedElement * last_element = nullptr;
 
     // return false if the list is empty
-    if(size(list) == 0)
+    if(list_size(list) == 0)
         return false;
 
-    if(element == NULL){
+    if(element == nullptr){
         *value = list->head->value;
         last_element = list->head;
         list->head = list->head->next;
 
-        if(size(list) ==1) list->tail == NULL;
+        if(list_size(list) ==1) list->tail == NULL;
 
     }else{
-        if(element->next == NULL) return false;
+        if(element->next == nullptr) return false;
 
         *value = element->next->value;
         last_element = element->next;
         element->next = element->next->next;
-        if(element->next == NULL) list->tail = element;
+        if(element->next == nullptr) list->tail = element;
     }
     free(last_element);
     list->size--;
