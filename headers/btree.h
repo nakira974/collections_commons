22b7285e07c127efae0fca84f5c46318c8b63726
nakira974 <maxime.loukhal@gmail.com>
2024-03-06 14:10:28 +0100

    Add doxygen doc build for cmake-build-debug, tests and doc build now only for Debug cmake build type

---
+++ headers/btree.h
@@ -0,0 +1,11 @@
+/**
+ * @file btree.h
+ * @brief This file contains the API for binary trees
+ * @author Maxime Loukhal
+ * @date 06/03/2024
+ */
+
+#ifndef COLLECTIONS_COMMONS_BTREE_H
+#define COLLECTIONS_COMMONS_BTREE_H
+
+#endif //COLLECTIONS_COMMONS_BTREE_H
