3f68804a82461b53ba7bfc26b0bed4e9ea953f7d
nakira974 <maxime.loukhal@gmail.com>
2024-03-12 23:21:48 +0100

    Update macros in collections_utils.h for concrete types

--- headers/collections_utils.h
+++ headers/collections_utils.h
@@ -188,6 +188,51 @@ static inline void **hashset_toArray(HashSet *set) {
     return dlist_toArray(set->elements);
 }
 
+/**
+ * @brief Inline function that evaluates the current queue into an double linked list
+ * @param queue Queue to be converted to array
+ * @return Converted queue to array
+ */
+static inline CLinkedList queue_toList(Queue queue){
+    return list_toCList(queue);
+}
+
+/**
+ * @brief Inline function that evaluates the current deque into a circular linked list
+ * @param deque Deque to be converted to array
+ * @return Converted queue to array
+ */
+static inline CLinkedList deque_toList(Deque deque){
+    return dlist_toCList(deque);
+}
+
+/**
+ * @brief Inline function that evaluates the current set into a circular linked list
+ * @param stack Set to be converted to a circular linked list
+ * @return Converted set to a circular linked list
+ */
+static inline CLinkedList set_toList(Set queue){
+    return list_toCList(set);
+}
+
+/**
+ * @brief Inline function that evaluates the current stack into a circular linked list
+ * @param stack Stack to be converted to a circular linked list
+ * @return Converted stack to a circular linked list
+ */
+static inline CLinkedList stack_toList(Stack stack){
+    return list_toCList(stack);
+}
+
+/**
+ * @brief Inline function that evaluates the current hashset into a circular linked list
+ * @param set Hashset to be converted to a circular linked list
+ * @return Converted hashset to a circular linked list
+ */
+static inline CLinkedList hashset_toList(HashSet set){
+    return dlist_toCList(set);
+}
+
 #else
 /**
  * @brief Macro that evaluates the current queue into an array
@@ -197,11 +242,25 @@ static inline void **hashset_toArray(HashSet *set) {
 #define queue_toArray(queue) list_toArray
 
 /**
+ * @brief Macro that evaluates the current queue into an double linked list
+ * @param queue Queue to be converted to array
+ * @return Converted queue to array
+ */
+#define queue_toList(queue) list_toCList
+
+/**
  * @brief Macro that evaluates the current deque into an array
  * @param deque Queue to be converted to array
  * @return Converted deque to array
  */
 #define deque_toArray(deque) dlist_toArray
+
+/**
+ * @brief Macro that evaluates the current deque into a circular linked list
+ * @param deque Deque to be converted to array
+ * @return Converted queue to array
+ */
+#define deque_toList(deque) dlist_toCList
 
 /**
  * @brief Macro that evaluates the current set into an array
@@ -211,6 +270,13 @@ static inline void **hashset_toArray(HashSet *set) {
 #define set_toArray(set) list_toArray
 
 /**
+ * @brief Macro that evaluates the current set into a circular linked list
+ * @param stack Set to be converted to a circular linked list
+ * @return Converted set to a circular linked list
+ */
+#define set_toList(set) list_toCList
+
+/**
  * @brief Macro that evaluates the current stack into an array
  * @param stack Stack to be converted to array
  * @return Converted stack to array
@@ -218,11 +284,25 @@ static inline void **hashset_toArray(HashSet *set) {
 #define stack_toArray(stack) list_toArray
 
 /**
+ * @brief Macro that evaluates the current stack into a circular linked list
+ * @param stack Stack to be converted to a circular linked list
+ * @return Converted stack to a circular linked list
+ */
+#define stack_toList(stack) list_toCList
+
+/**
  * @brief Macro that evaluates the current hashset into an array
  * @param set Hashset to be converted to array
  * @return Converted hashset to array
  */
 #define hashset_toArray(set) dlist_toArray((set)->elements)
+
+/**
+ * @brief Macro that evaluates the current hashset into a circular linked list
+ * @param set Hashset to be converted to a circular linked list
+ * @return Converted hashset to a circular linked list
+ */
+#define hashset_toList(set) dlist_toCList((set)->elements)
 #endif
 
 
